8c2e004ed90a198af5b6f0155c661f410875d127
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-09 19:37:42 +0000

    build.sh: provided with image auto-cleanup command

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,3 +12,11 @@ cd base/docker
 docker build -t ${REPO}:${TS} -f Dockerfile.base .
 
 docker tag ${REPO}:${TS} ${REPO}:latest
+
+{ set +x; } 2>/dev/null
+
+docker image ls --format json |
+  jq -r '. | select(.Repository == "'"${REPO}"'" and .Tag != "latest") | .Tag' |
+  sort -r |
+  awk -v REPO="$REPO" '{ if (NR>1) printf("docker rmi %s:%s\n",REPO,$0) }' |
+  bash -x
