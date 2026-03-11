e12f7da5c989bf5ed2cb10e9d292ff2dd125f1ec
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-11 16:14:41 +0000

    run.sh: 'docker run' uid/gid arguments adapted

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,8 @@
 IMAGE="ghcr.io/sfmunoz/jails-claude-code-plain:latest"
 if [ "$JAILS_ROOT" = "1" ]; then
   set -x
-  exec docker run -it --rm --name claude-code "${IMAGE}" bash
+  exec docker run -it --rm --name claude-code -u root:root "${IMAGE}" bash
 else
   set -x
-  exec docker run -it --rm --name claude-code -v .:/workspace -u $(id -u):$(id -g) "${IMAGE}" bash
+  exec docker run -it --rm --name claude-code -v .:/workspace "${IMAGE}" bash
 fi
