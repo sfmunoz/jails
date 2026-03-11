363c96ddcf44445bfc915f27d476fd6231862028
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-11 17:39:27 +0000

    run.sh: command to execute can be specified as argument

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,12 @@
 IMAGE="ghcr.io/sfmunoz/jails-claude-code-plain:latest"
 if [ "$JAILS_ROOT" = "1" ]; then
   set -x
-  exec docker run -it --rm --name claude-code -u root:root "${IMAGE}" bash
+  exec docker run -it --rm --name claude-code -u root:root "${IMAGE}" "$@"
 else
   HOME_JAILS="${HOME}/.jails"
   set -x
   mkdir -p "${HOME_JAILS}"
   rm -rf "${HOME_JAILS}/.local"
   ln -s ../jails_local "${HOME_JAILS}/.local"
-  exec docker run -it --rm --name claude-code -v "${HOME_JAILS}:/home/jails:rw" -v ".:/workspace:rw" "${IMAGE}" bash
+  exec docker run -it --rm --name claude-code -v "${HOME_JAILS}:/home/jails:rw" -v ".:/workspace:rw" "${IMAGE}" "$@"
 fi
