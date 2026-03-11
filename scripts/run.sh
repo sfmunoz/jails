25fc82c677ff8378521449c515509f150c01762e
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-11 17:12:04 +0000

    run.sh: '<HOME>/.jails' mounted as '/home/jails' for persistency

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,10 @@ if [ "$JAILS_ROOT" = "1" ]; then
   set -x
   exec docker run -it --rm --name claude-code -u root:root "${IMAGE}" bash
 else
+  HOME_JAILS="${HOME}/.jails"
   set -x
-  exec docker run -it --rm --name claude-code -v .:/workspace "${IMAGE}" bash
+  mkdir -p "${HOME_JAILS}"
+  rm -rf "${HOME_JAILS}/.local"
+  ln -s ../jails_local "${HOME_JAILS}/.local"
+  exec docker run -it --rm --name claude-code -v "${HOME_JAILS}:/home/jails:rw" -v ".:/workspace:rw" "${IMAGE}" bash
 fi
